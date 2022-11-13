84488397312cb8e20e673aca2e385542685a5877
blaqbrane <nnamanipeter58@gmail.com>
2022-11-13 01:10:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Locations from "./Components/Locations";
+import Boarding from "./Components/Boarding";
+import Container from "./Components/Container";
+import Slider from "./Components/Slider";
+import Footer from "./Components/Footer";
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <About/>
+     <Locations/>
+     <Boarding/>
+     <Container/>
+     <Slider/>
+     <Footer/>
+    </>
+  );
+}
+export default App;
